f8ea1cb2287bd6795ddb53980b9a26543d301ad4
George Kuzora <77954822+GeorgeKuzora@users.noreply.github.com>
2021-03-17 20:46:38 +0300

    Added main module

--- kb1520_app/lib.py
+++ kb1520_app/lib.py
@@ -1,124 +1,52 @@
-'''Библиотека модулей для программы по подготовке списка материалов для продукта проходящего ремонт'''
+'''Библиотека модулей для программы по подготовке списка материалов для продукта
+проходящего ремонт'''
 
 import pandas as pd
 import numpy as np
 import openpyxl
 
 class Data_list:
-    '''Класс для загузки данных о расходе материалов в прошлом и существующих продуктах и их свойствах. Требует подготовленных и очищенных данных в формате .xlsx.'''
+    '''Класс для загузки данных о расходе материалов в прошлом и существующих
+    компонентах и их свойствах. Требует подготовленных и очищенных данных в формате .xlsx.'''
     
-    def __init__(self, basic_file, plant_file, zparts_file, product_file):
+    def __init__(self, basic_file):
+        '''Атрибут имя файла .xlsx где храняться данные разделенные по вкладкам,
+        сформированныи при промощи Powerquery'''
         self.basic_file = basic_file
-        self.plant_file = plant_file
-        self.zparts_file = zparts_file
-        self.product_file = product_file
-#        self.data_df = None
-#        self.basic_df = None
-#        self.plant_df = None
-#        self.zparts_df = None
-#        self.product_df = None
-#        self.components_df = None
 
-    def create_basic(self):
-        '''Создает датафрейм из листа Эксель'''
+    def create_materials(self):
+        '''Создает датафрейм из листа Эксель в котором содержатся данные о
+        существующих материалах и их свойствах.'''
         with pd.ExcelFile(self.basic_file) as xlsx:
-            self.basic_df = pd.read_excel(xlsx, "basic_query", na_values=["NA"], index_col=0, usecols="A:P")
-        return self.basic_df
-
-    def create_plant(self):
-        '''Создает датафрейм из листа Эксель'''
-        with pd.ExcelFile(self.plant_file) as xlsx:
-            self.plant_df = pd.read_excel(xlsx, "plant_query", na_values=["NA"], index_col=0, usecols="A:E")
-        return self.plant_df
+            self.materials_df = pd.read_excel(xlsx, "materials_combined", na_values=["NA"], index_col=0, usecols="A:E")
+        return self.materials_df
 
     def create_zparts(self):
-        '''Создает датафрейм из листа Эксель'''
-        with pd.ExcelFile(self.zparts_file) as xlsx:
-            self.zparts_df = pd.read_excel(xlsx, "zparts_query", na_values=["NA"], usecols="B:Z")
+        '''Создает датафрейм из листа Эксель в котором содержаться данные о
+        произведенных ремонтах и расходе материалов в этих ремонтах.'''
+        with pd.ExcelFile(self.basic_file) as xlsx:
+            self.zparts_df = pd.read_excel(xlsx, "zparts_combined", na_values=["NA"], usecols="A:U")
         return self.zparts_df
 
-    def create_product(self):
-        '''Создает датафрейм из листа Эксель'''
-        with pd.ExcelFile(self.product_file) as xlsx:
-            self.product_df = pd.read_excel(xlsx, "product_query", na_values=["NA"], index_col=0, usecols="A:K")
-        return self.product_df
-
-    def connect_components(self):
-        '''Соединяет два датафрейма и получает датафрейм с данными о используемых компонентах'''
-        frames = [self.basic_df, self.plant_df]
-        self.components_df = pd.concat(
-                                    frames,
-                                    axis=1,
-                                    join="outer",
-                                    ignore_index=False,
-                                    )
-        return self.components_df
-
-    def connect_repairs(self):
-        '''Соединяет два датафрейма и получает датафрейм с данными о проведенных ремонтах и расходе материалов'''
-        self.repairs_df = pd.merge(
-                                self.zparts_df,
-                                self.product_df,
-                                how="left",
-                                on=None,
-                                left_on="Material",
-                                right_on="Service material",
-                                left_index=False,
-                                right_index=True,
-                                sort=False,
-                                suffixes=("_x", "_y"),
-                                copy=True,
-                                indicator=False,
-                                validate=None,
-                                )
-        self.repairs_df = pd.merge(
-                                self.repairs_df,
-                                self.components_df,
-                                how="left",
-                                on=None,
-                                left_on="Component",
-                                right_on="B~MATNR",
-                                left_index=False,
-                                right_index=True,
-                                sort=False,
-                                suffixes=("_x", "_y"),
-                                copy=True,
-                                indicator=False,
-                                validate=None,
-                                )
-        return self.repairs_df
-
-    def connect_service(self):
-        '''Соединяет два датафрейма и получает датафрейм с данными о проведенных ремонтах и расходе материалов'''
-        self.service_df = pd.merge(
-                                self.product_df,
-                                self.basic_df,
-                                how="left",
-                                on=None,
-                                left_on="Material entered",
-                                right_on="B~MATNR",
-                                left_index=False,
-                                right_index=True,
-                                sort=False,
-                                suffixes=("_x", "_y"),
-                                copy=True,
-                                indicator=False,
-                                validate=None,
-                                )
-        return self.service_df
 
 class User_data:
-    '''Класс для загрузки и обработки данных загружаеммых пользователем, а именно ВОМ материала. Принимает в качестве атрибута имя файла со списком материалов. Требует очистки данных пользователя'''
+    '''Класс для загрузки и обработки данных загружаеммых пользователем,
+    а именно ВОМ материала и данные о продукте занесенные в пользовательский файл.
+    Требует очистки данных пользователя'''
 
-    def __init__(self, file_name, components_df):
+    def __init__(self, file_name):
+        '''Атрибут имя файла .xlsx где храняться данные разделенные по вкладкам,
+        сформированныи при промощи Powerquery'''
         self.file_name = file_name
-        self.components_df = components_df
+        self.coef_array = []
 
     def create_df(self):
-        '''Этот метод создает датафрейм pandas и расчитывает реальное количество компонентов в листе материалов. Реальное количество компонентов расчитывается на основании структуры уровней материалов входящих друг в друга и их количеств. Для расчета использована структура stack.''' 
+        '''Этот метод создает датафрейм для материалов входящих в БОМ для которого
+        производится расчет потребностей, и расчитывает реальное количество
+        компонентов в листе материалов.''' 
 
         with pd.ExcelFile(self.file_name) as xlsx:
-            df = pd.read_excel(xlsx, "BOM", usecols=[0,1,2,3,4,5,6,7,8,9,10])
+            df = pd.read_excel(xlsx, "Default", usecols="A:J")
         mult_index = []
         mult_list = []
         for i in range(len(df)):
@@ -141,14 +69,66 @@ class User_data:
         self.user_df["True quantity"] = (self.user_df["Quantity"] * self.user_df["Multiply"])
         return self.user_df
 
-    def connect_df(self):
-        self.user_df = pd.merge(
-                                self.user_df,
-                                self.basic_df,
+    def create_prdinfo(self):
+        '''Создает датафрейм данных о ремонтируемом продукте из листа Эксель,
+        загружаемого пользователем. Данные о продукте вносятся пользователем во 
+        вкладку info'''
+        with pd.ExcelFile(self.file_name) as xlsx:
+            self.prdinfo_df = pd.read_excel(xlsx, "info", na_values=["NA"], index_col=0, usecols="A:C")
+        return self.prdinfo_df
+
+    def create_coef_array(self):
+        '''Добавляет расчитанный для объекта компонента, входящего в лист с
+        пользовательскими данными, через который идет итерация, в лист для
+        последующего добвавления к данным'''
+        self.coef_array.append(Complete_df.calculate_coef(self))
+    
+    def coef_arr_concat(self):
+        '''Присоединяет лист к добавленными коэффициентами к основному дата фрейму
+        пользовательских данных'''
+        coef_arr_s = pd.Series(self.coef_array, name="Coef")
+        self.user_df = pd.concat([df, coef_arr_s], axis=1)
+        return self.user_df
+
+    def print_df(self):
+        '''Записывает пользовательский датафрейм в новый эксель файл'''
+        self.user_df.to_excel("true_" + self.file_name, sheet_name="BOM")
+
+
+class Material_obj:
+    '''Общий класс для создаваеммых объектов компонентов и продуктов, так как
+    они разделяют много схожих параметров, мы воспользуемся принципом наследственности.'''
+
+    def __init__(self, index, user_db_name, data_db_name):
+        '''Атрибуты:
+           Индекс строки в исходных данных для продукта это
+           будет 0, так как в данных одна строчка, для компонента это будет
+           индекс строки в датафрейме пользовательских данных через который идет
+           итерация.
+           Имя данных пользователя: для продукта это имя датафрейма prdinfo_df,
+           для компонента это имя датафрейма user_df.
+           Имя данных о материале: это имя данных датафрейма materials_df.'''
+        self.index = index
+        self.user_db_name = user_db_name
+        self.data_db_name = data_db_name
+
+    def prepare_objdata(self):
+        '''Метод создает новый объект в формате pd.df, из строки заданных данных 
+        пользователя (либо одна строка для продукта, либо строка через которую
+        проходит итерация для компонента'''
+        self.objdata_df = user_db_name.iloc(self.index)
+        return self.objdata_df
+
+    def create_obj(self):
+        '''Метод создает новый датафрейм на основании данных пользователя(для
+        конкретной строки и общих данных о всех материалах.'''
+        self.obj_df = pd.merge(
+                                self.objdata_df,
+                                self.data_db_name,
                                 how="left",
                                 on=None,
                                 left_on="Number",
-                                right_on="B~MATNR",
+                                right_on="Material",
                                 left_index=False,
                                 right_index=True,
                                 sort=False,
@@ -157,30 +137,97 @@ class User_data:
                                 indicator=False,
                                 validate=None,
                                 )
-        return self.user_df
+        return self.obj_df
 
 
-    
+class Component_obj(Material_obj):
+    '''Класс дочерний к классу материала, нужен для создания конкретного объекта
+    компонента'''
+
+    def rename_obj(self):
+        '''Метод переименовывает необходимые столбцы в датафрейме компонента,
+        для дальнейшего удобства'''
+        self.obj_df.rename({"Number": "Component",
+                            "Material type": "Component material type",
+                            "Material COC": "Component COC",
+                            "Material shelf life": "Component shelf life",
+                            "Material description": "Component description"}, axis="columns")
+        return self.obj_df
+
+class Product_obj(Material_obj):
+    '''Класс дочерний к классу материала, нужен для создания конкретного объекта
+    продукта'''
+
+    def rename_obj(self):
+        '''Метод переименовывает необходимые столбцы в датафрейме продукта,
+        для дальнейшего удобства'''
+        self.obj_df.rename({"Number": "Product",
+                            "Material type": "Product material type",
+                            "Material COC": "Product COC",
+                            "Material shelf life": "Product shelf life",
+                            "Material description": "Product description"}, axis="columns")
+        return self.obj_df
+        
+
+class Complete_df:
+    '''Класс для окончательного расчета данных о коэффициенте для каждого из
+    компонентов в пользовательских данных, обладает атрибутом класса - словарем
+    с данными о названиях столбцов через которые будет идти расширение выборки'''
+    spec_dict = {1: ("Component", "Service material"),
+                 2: ("Component", "Product"),
+                 3: ("Component", "Product description"),
+                 4: ("Component", "Product material type"),
+                 5: ("Component", "Product COC"),
+                 6: ("Component description", "Service material"),
+                 7: ("Component description", "Product"),
+                 8: ("Component description", "Product description"),
+                 9: ("Component description", "Product material type"),
+                 10: ("Component description", "Product COC")
+                }
+
+    def __init__(self, zparts_df, component_obj, product_obj):
+        '''Атрибуты:
+            Данные о ремонтах: данные из датафрейма zparts_df,
+            Объект компонента: объект компонента для каждого из материалов в
+            пользовательских данных,
+            Объект продукта: объект продукта для которого производиться ремонт'''
+        self.zparts_df = zparts_df
+        self.component_obj = component_obj
+        self.product_obj = product_obj
+
+    def join_obj(self):
+        '''Метод соединяет объеты компонента и продукта в один датафрейм,
+        для того чтобы было проще создавать выборку из данных о ремонтах'''
+        frames = [self.component_obj, self.product_obj]
+        self.joined_obj = pd.concat(frames, axis=1)
+        return self.joined_obj
+
+    def create_query(self):
+        '''Метод итерирует через данные словаря для расширения выборки. Для
+        каждой из итераций создает выборку с данными о ремонтах продуктов и
+        колличестве потребленного компонента в данных ремонтах. При наличии более
+        20 ремонтов в выборке, возвращает созданную на данном шаге выборку.'''
+        for key: value in spec_dict:
+            current_frame = self.joined_obj[value]
+            current_dict = current_frame.to_dict('records')
+            self.current_query = self.zparts_df[(self.zparts_df[value[0]] == current_dict[value[0]]) & (self.zparts_df[value[1]] == current_dict[value[1]])]
+            sum_qb = current_query["Quantity Balance"].sum()
+            if sum_qb >= 20:
+                break
+        return self.current_query
+
+    def calculate_coef(self):
+        '''Расчитывает коэффициент на основании данных о расходе коэффициента,
+        в случае если потребления материалов не случилось для данных ремонтов,
+        расчитывает на основании существующих коэффициентов'''
+        if self.current_query["C_consum"].sum() == 0:
+            self.coef = self.current_query["C quantity"].min()
+        else:
+            self.coef = self.current_query["Prob_C"].mean()
+        return self.coef
+            
+
+
+
 if __name__ == '__main__':
-
-    a = Data_list(
-            "/home/georgiy/Documents/Code/kb1520_app/Excel/data/query/basic_query.xlsx",
-            "/home/georgiy/Documents/Code/kb1520_app/Excel/data/query/plant_query.xlsx",
-            "/home/georgiy/Documents/Code/kb1520_app/Excel/data/query/zparts_query.xlsx",
-            "/home/georgiy/Documents/Code/kb1520_app/Excel/data/query/product_query.xlsx",
-            )
-    a.create_basic()
-    a.create_plant()
-    a.create_zparts()
-    a.create_product()
-
-    print(a.zparts_df)
-    print(a.product_df)
-    a.connect_components()
-    a.connect_repairs()
-
-    print(a.repairs_df)
-
-#    b = User_data("/home/georgiy/Documents/Code/kb1520_app/Excel/II78917_2_O.xlsx")
-#    b.create_df()
-#    print(b.user_df)
+    pass
---
+++ kb1520_app/main.py
@@ -0,0 +1,30 @@
+import lib
+
+data_file_name = "RU05_query.xlsx"
+user_file_name = input("Please enter a file name: ")
+
+data_list = lib.Data_list(data_file_name)
+mat_list = data_list.create_materials()
+zparts_list = data_list.create_zparts()
+
+user_data = lib.User_data(user_file_name)
+user_list = user_data.create_df()
+prdinfo = user_data.create_prdinfo()
+
+product = lib.Product_obj(0, prdinfo, mat_list)
+product.prepare_objdata()
+product.create_obj()
+product_obj = product.rename_obj()
+
+for i in range(len(user_list)):
+    component = lib.Component_obj(i, user_list, mat_list)
+    component.prepare_objdata()
+    component.create_obj()
+    component_obj = component.rename_obj()
+    complete_df = lib.Complete_df(zparts_list, component_obj, product_obj)
+    complete_df.join_obj()
+    complete_df.create_query()
+    user_data.create_coef_array()
+
+user_data.coef_arr_concat()
+user_data.print_df()
